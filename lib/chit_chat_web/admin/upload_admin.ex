ae30fc6476690fa681b40bfd133dc8bf051ff91c
areski <areski@gmail.com>
2020-06-08 17:16:22 +0200

    [fix](document) WIP upload files + KAffy

--- lib/chit_chat_web/admin/upload_admin.ex
+++ lib/chit_chat_web/admin/upload_admin.ex
@@ -1,6 +1,28 @@
 defmodule ChitChatWeb.UploadAdmin do
   alias ChitChat.Documents
 
+
+  # def before_create(conn, changeset) do
+  #   %Plug.Conn{params: %{"upload" => %{"filename" => %Plug.Upload{}=upload }}} = conn
+  #   IO.inspect("before_create===============")
+  #   IO.inspect(conn)
+  #   IO.inspect(upload)
+  #   Documents.create_upload_from_plug_upload(upload)
+  # end
+
+  # def create(conn, %{"upload" => %Plug.Upload{}=upload}) do
+  #   case Documents.create_upload_from_plug_upload(upload) do
+
+  #     {:ok, upload}->
+  #       put_flash(conn, :info, "file uploaded correctly")
+  #       redirect(conn, to: Routes.upload_path(conn, :index))
+
+  #     {:error, reason}->
+  #       put_flash(conn, :error, "error upload file: #{inspect(reason)}")
+  #       render(conn, "new.html")
+  #   end
+  # end
+
   def index(_) do
     [
       id: nil,
